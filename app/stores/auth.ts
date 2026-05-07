676d90e5c989ea2421e836d29eb3976d92950b0b
neel4os <neel4os@gmail.com>
2026-05-07 19:49:21 +0530

    before caching user

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { API_BASE } from "../lib/api";
 import { CachedUser } from "../lib/offline";
+import { devtools } from 'zustand/middleware'
 
 
 type AuthStatus = "authenticated" | "unauthenticated" | "checking";
@@ -12,27 +13,29 @@ export interface AuthState {
   clearAuth: () => void;
 }
 
-export const useAuthStore = create<AuthState>((set, get) => ({
-  status: "checking",
-  user: null,
-  initialize: async () => {
-    try {
-      const res = await fetch(`${API_BASE}/api/v0/auth/me`, {
-        credentials: "same-origin",
-      });
-      if (res.ok) {
-        const user: CachedUser = await res.json();
-        set({ status: "authenticated", user });
-        //await cacheUser(user);
-      } else {
+export const useAuthStore = create<AuthState>()(
+  devtools((set, get) => ({
+    status: "checking",
+    user: null,
+    initialize: async () => {
+      try {
+        const res = await fetch(`${API_BASE}/api/v0/auth/me`, {
+          credentials: "include",
+        });
+        if (res.ok) {
+          const user: CachedUser = await res.json();
+          set({ status: "authenticated", user });
+          //await cacheUser(user);
+        } else {
+          set({ status: "unauthenticated", user: null });
+        }
+      } catch (error) {
+        console.log("Error in authStore");
         set({ status: "unauthenticated", user: null });
       }
-    } catch (error) {
-      console.log("Error in authStore");
-      set({ status: "unauthenticated", user: null });
-    }
-  },
-  clearAuth: ()=>{
-    set({status: "unauthenticated", user: null})
-  },
-}));
+    },
+    clearAuth: ()=>{
+      set({status: "unauthenticated", user: null})
+    },
+  }), { name: 'AuthStore' })
+);
